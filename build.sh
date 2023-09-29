906b37c34e2f299fc4ed636f02e956a07ca65a21
grez-lucas <lgrez1@miuandes.cl>
2023-09-29 12:15:22 -0300

    feat: deploy.yaml working with frontend and classnotes API

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 
 cd sign_up_api_basic
-echo Building API App
+echo Building Users API App
 sudo docker build -t app-users:1.0 .
 cd ..
 
 cd courses_api
-echo Building SA web App	
+echo Building Courses API App	
 sudo docker build -t app-courses:1.0 .
 cd ..
+
+cd classnotes_api
+echo Building Classnotes API App
+sudo docker build -t app-classnotes:1.0 .
+cd ..
+
+cd frontend_react/my-react-app
+echo Building Frontend App
+sudo docker build -t app-frontend:1.0 .
+cd ../..
 
 echo Checking process
 sudo docker ps
