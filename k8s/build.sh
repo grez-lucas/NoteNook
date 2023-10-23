d03d8e1d25f3f3c60ae6e89fe5088a89a410c25a
grez-lucas <lucasgrez3010@gmail.com>
2023-10-23 13:44:31 -0300

    feat: k8s script for signup_deployment

--- k8s/build.sh
+++ k8s/build.sh
@@ -1,7 +1,12 @@
 minikube start --driver docker
+
 minikube kubectl -- apply -f postgres/postgres-pvc.yaml
 minikube kubectl -- apply -f postgres/postgres-config.yaml
 minikube kubectl -- apply -f postgres/postgres-secret.yaml
 minikube kubectl -- apply -f postgres/postgres.yaml
+
 minikube kubectl -- apply -f classnotes/classnotes-secret.yaml
 minikube kubectl -- apply -f classnotes/classnotes.yaml
+
+minikube kubectl -- apply -f users/users-secret.yaml
+minikube kubectl -- apply -f users/users.yaml
